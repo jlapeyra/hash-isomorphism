6aa73fea4bde86b03c33c86ce0933778be4ba4a2
jlapeyra <jlapeyraa@gmail.com>
2024-12-04 23:16:03 +0100

    long -> long long

--- src/utils.cc
+++ src/utils.cc
@@ -8,7 +8,7 @@
 
 
 typedef unsigned int uint;
-typedef unsigned long ulong;
+typedef unsigned long long ulong;
 
 /*class Printable {
     public: virtual std::ostream &print(std::ostream &os=std::cout) const = 0;
@@ -53,25 +53,30 @@ auto since(std::chrono::time_point<clock_t, duration_t> const& start)
 }
 
 //Mersenne primes
-const ulong MERSENNE_19 = (2 << 19) - 1;
-const ulong MERSENNE_31 = (2 << 31) - 1;
-const ulong MERSENNE_61 = (2 << 61) - 1;
+const ulong MERSENNE_19 = (2ULL << 19) - 1;
+const ulong MERSENNE_31 = (2ULL << 31) - 1;
+const ulong MERSENNE_61 = (2ULL << 61) - 1;
+
+const ulong INF = ULLONG_MAX;
+
+
+bool multOverflow(ulong a, ulong b) {
+    //if (a == INF || b == INF) return true;
+    if (b != 0 && a > INF / b) return true;
+    if (a != 0 && b > INF / a) return true;
+    return false;
+}
 
 ulong factorial(uint n) {
     ulong result = 1;
     for (ulong i = 1; i <= n; i++) {
-        if (multOverflow(result, i)) return LONG_MAX;
+        if (multOverflow(result, i)) return INF;
         else result *= i;
     }
     return result;
 }
 
-bool multOverflow(ulong a, ulong b) {
-    //if (a == LONG_MAX || b == LONG_MAX) return true;
-    if (b != 0 && a > LONG_MAX / b) return true;
-    if (a != 0 && b > LONG_MAX / a) return true;
-    return false;
-}
+
 
 
 #endif
